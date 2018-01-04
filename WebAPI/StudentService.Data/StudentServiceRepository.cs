caad6b06d5dd4cb411a23740cbaf976a73ee00fc
Jake Coddaire <Jake.Coddaire@meijer.com>
2018-01-04 17:10:15 -0500

    Implemented some data methods. Now you can do things with departments. Neat, eh?

--- WebAPI/StudentService.Data/StudentServiceRepository.cs
+++ WebAPI/StudentService.Data/StudentServiceRepository.cs
@@ -23,13 +23,12 @@ namespace StudentService.Data
                 return _Database;
             }
         }
-
-
-        #region People Methods
         public StudentServiceRepository()
         {
 
         }
+
+        #region People Methods
 
         /// <summary>
         /// Gets all people.
@@ -118,7 +117,7 @@ namespace StudentService.Data
             var changedPeep = Database.People.Where(p => p.PersonID == person.PersonID).FirstOrDefault();
             if(changedPeep == null || changedPeep.PersonID != person.PersonID)
             {
-                throw new KeyNotFoundException("Could not find a matching ID in the dataset.");
+                throw new KeyNotFoundException("Could not find a matching item in the dataset.");
             }
 
             changedPeep.FirstName = person.FirstName;
@@ -165,27 +164,104 @@ namespace StudentService.Data
 
         public IEnumerable<DepartmentDTO> GetAllDepartments()
         {
-            throw new NotImplementedException();
+            var source = Database.Departments.Select(
+                x => new DepartmentDTO()
+                {
+                    DepartmentID = x.DepartmentID,
+                    Name = x.Name,
+                    Budget = x.Budget,
+                    StartDate = x.StartDate,
+                    Administrator = x.Administrator
+
+                }).ToList();
+
+            return source;
         }
 
         public DepartmentDTO GetDepartment(int departmentID)
         {
-            throw new NotImplementedException();
+            if (departmentID <= 0)
+            {
+                return null;
+            }
+
+            var target = Database.Departments.Where(d => d.DepartmentID == departmentID).Select(
+                x => new DepartmentDTO()
+                {
+                    DepartmentID = x.DepartmentID,
+                    Name = x.Name,
+                    Budget = x.Budget,
+                    StartDate = x.StartDate,
+                    Administrator = x.Administrator
+
+                }).FirstOrDefault();
+
+            if (target != null && target.DepartmentID > 0)
+            {
+                return target;
+            }
+
+            return null;
         }
 
         public DepartmentDTO CreateDepartment(DepartmentDTO department)
         {
-            throw new NotImplementedException();
+            var topID = GetAllDepartments().Max(x => x.DepartmentID) + 1;
+
+            var obj = new Department
+            {
+                DepartmentID = topID,
+                Name = department.Name,
+                Budget = department.Budget,
+                StartDate = department.StartDate,
+                Administrator = department.Administrator
+            };
+
+            Database.Departments.Add(obj);
+            Database.SaveChanges();
+
+            department.DepartmentID = topID;
+
+            return department;
         }
 
         public DepartmentDTO UpdateDepartment(DepartmentDTO department)
         {
-            throw new NotImplementedException();
+            var changed = Database.Departments.Where(d => d.DepartmentID == department.DepartmentID).FirstOrDefault();
+            if (changed == null || changed.DepartmentID != department.DepartmentID)
+            {
+                throw new KeyNotFoundException("Could not find a matching item in the dataset.");
+            }
+
+            changed.Name = department.Name;
+            changed.Budget = department.Budget;
+            changed.StartDate = department.StartDate;
+            changed.Administrator = department.Administrator;
+
+            Database.Departments.Attach(changed);
+
+            var entry = Database.Entry(changed);
+            entry.Property(e => e.Name).IsModified = true;
+            entry.Property(e => e.Budget).IsModified = true;
+            entry.Property(e => e.StartDate).IsModified = true;
+            entry.Property(e => e.Administrator).IsModified = true;
+
+            Database.SaveChanges();
+
+            return department;
         }
 
         public int DeleteDepartment(int departmentID)
         {
-            throw new NotImplementedException();
+
+            var target = Database.Departments.Where(x => x.DepartmentID == departmentID).FirstOrDefault();
+
+            if (target != null && target.DepartmentID == departmentID)
+            {
+                Database.Departments.Remove(target);
+                return Database.SaveChanges();
+            }
+            return -1;
         }
         #endregion
 
